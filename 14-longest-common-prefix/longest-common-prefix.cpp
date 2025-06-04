ee3a84c5a75c381c5932604906b155e6a7f8c454
Shrayash awasthi <awasthishrayashofc@gmail.com>
2025-06-04 19:28:18 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (19.99%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int temp =0;
-        
-        string ans ="";
-        if(strs.size()==0  || strs[0].size()==0 ){
-            return ans;
-        }
-        if(strs.size()==1){
-            return strs[0];
-        }
-        if(strs[0].front()!=strs[1].front()){
-            return ans;
-        }
-        for(int i=0;i<strs.size()-1;i++){
-            while(temp<min(strs[i].length(),strs[i+1].length())){
-                if(strs[i][temp]==strs[i+1][temp]){
-                    ans+=strs[i][temp];
-                }
-                else{
-                    break;
-                }
-                temp++;
+        // firstly we are going to sort the vector of string so that we get the element with longest common prefix together 
+        sort(strs.begin(),strs.end());
+
+        // now we are going to check for the prefix from the first and last element of the vector because we will get longest common prefix from all the elements of array from it
+        int i=0;string ans = "";
+        string start = strs[0];
+        string last = strs[strs.size()-1];
+        while(i<min(start.size(),last.size())){
+            if(start[i]!=last[i]){
+                break;
             }
-            strs[i+1]=ans;
-            temp=0;
-            if(i!=strs.size()-2){
-                ans="";
-            }
-        }
+            ans+=start[i];
+            i++;
+        } 
         return ans;
-        
     }
-    
 };
